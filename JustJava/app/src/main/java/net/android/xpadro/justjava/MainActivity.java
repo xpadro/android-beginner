2eebc5c3c91bd3b03aa81b3fa5b136ca312b17e0
Xavier Padro <xpadro.sw@gmail.com>
2015-07-12 16:03:49 +0200

    Update prices implementation

--- JustJava/app/src/main/java/net/android/xpadro/justjava/MainActivity.java
+++ JustJava/app/src/main/java/net/android/xpadro/justjava/MainActivity.java
@@ -1,12 +1,18 @@
 package net.android.xpadro.justjava;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
 
+import java.text.NumberFormat;
 
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends ActionBarActivity {
+    private int quantity = 2;
+    private int price = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,25 +20,44 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        displayPrice();
+        updateOrderMessage("Thank you!");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void increment(View view) {
+        display(++quantity);
+        displayPrice();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    public void decrement(View view) {
+        display(--quantity);
+        displayPrice();
+    }
 
-        return super.onOptionsItemSelected(item);
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+        updateOrderMessage("");
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice() {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(quantity * price));
+    }
+
+    private void updateOrderMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.order_text_view);
+        priceTextView.setText(message);
     }
 }
