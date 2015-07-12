78dd162e693b14c48b76b0d0aeb4f51c2ec8e370
Xavier Padro <xpadro.sw@gmail.com>
2015-07-12 22:36:36 +0200

    Included submit order message

--- JustJava/app/src/main/java/net/android/xpadro/justjava/MainActivity.java
+++ JustJava/app/src/main/java/net/android/xpadro/justjava/MainActivity.java
@@ -1,7 +1,7 @@
 package net.android.xpadro.justjava;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,7 +10,7 @@ import java.text.NumberFormat;
 /**
  * This app displays an order form to order coffee.
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
     private int quantity = 2;
     private int price = 5;
 
@@ -20,44 +20,46 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * This method is called when the order button is clicked.
-     */
-    public void submitOrder(View view) {
-        displayPrice();
-        updateOrderMessage("Thank you!");
-    }
-
     public void increment(View view) {
-        display(++quantity);
-        displayPrice();
+        displayQuantity(++quantity);
+        displayPrice(calculatePrice());
     }
 
     public void decrement(View view) {
-        display(--quantity);
-        displayPrice();
+        displayQuantity(--quantity);
+        displayPrice(calculatePrice());
+    }
+
+    public void submitOrder(View view) {
+        displayPrice(createOrderSummary());
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-        updateOrderMessage("");
     }
 
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice() {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(quantity * price));
+    private void displayPrice(String price) {
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        priceTextView.setText(price);
     }
 
-    private void updateOrderMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.order_text_view);
-        priceTextView.setText(message);
+    private String createOrderSummary() {
+        String price = calculatePrice();
+        return "Name: Xavi Padro\nQuantity: " + quantity + "\nTotal: " + price + "\nThank you!";
+    }
+
+    /**
+     * Calculates the price of the order.
+     */
+    private String calculatePrice() {
+        return NumberFormat.getCurrencyInstance().format(quantity * price);
     }
 }
