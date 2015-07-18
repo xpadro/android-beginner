6c310daf408e367fc93f481a98fd748a1294711f
Xavier Padro <xpadro.sw@gmail.com>
2015-07-18 11:46:37 +0200

    localized app, added bounds

--- JustJava/app/src/main/java/net/android/xpadro/justjava/MainActivity.java
+++ JustJava/app/src/main/java/net/android/xpadro/justjava/MainActivity.java
@@ -1,11 +1,14 @@
 package net.android.xpadro.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -23,17 +26,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increment(View view) {
+        if (quantity == 100) {
+            Toast.makeText(this, getString(R.string.msg_upper_limit), Toast.LENGTH_SHORT).show();
+            return;
+        }
         displayQuantity(++quantity);
         displayPrice(calculatePrice());
     }
 
     public void decrement(View view) {
+        if (quantity == 1) {
+            Toast.makeText(this, getString(R.string.msg_lower_limit), Toast.LENGTH_SHORT).show();
+            return;
+        }
         displayQuantity(--quantity);
         displayPrice(calculatePrice());
     }
 
     public void submitOrder(View view) {
-        displayPrice(createOrderSummary());
+        sendOrderByMail();
     }
 
     /**
@@ -54,29 +65,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String createOrderSummary() {
-        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.cream_checkbox);
-        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
-
-        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
-        boolean hasChocolate = chocolateCheckBox.isChecked();
+        boolean hasWhippedCream = isWhippedCreamSelected();
+        boolean hasChocolate = isChocolateSelected();
 
         EditText nameInput = (EditText) findViewById(R.id.input_name_view);
         String name = nameInput.getText().toString();
 
         String price = calculatePrice();
 
-        return "Name: " + name + "\n" +
-                "Add whipped cream? " + hasWhippedCream + "\n" +
-                "Add Chocolate? " + hasChocolate + "\n" +
-                "Quantity: " + quantity + "\n" +
-                "Total: " + price + "\n" +
-                "Thank you!";
+        return getString(R.string.order_name, name) + "\n" +
+                getString(R.string.order_cream, hasWhippedCream) + "\n" +
+                getString(R.string.order_chocolate, hasChocolate) + "\n" +
+                getString(R.string.order_quantity, quantity) + "\n" +
+                getString(R.string.order_total, price) + "\n" +
+                getString(R.string.order_thanks);
     }
 
     /**
      * Calculates the price of the order.
      */
     private String calculatePrice() {
-        return NumberFormat.getCurrencyInstance().format(quantity * price);
+        int calculatedPrice = price;
+        if (isWhippedCreamSelected()) calculatedPrice += 1;
+        if (isChocolateSelected()) calculatedPrice += 2;
+
+        return NumberFormat.getCurrencyInstance().format(quantity * calculatedPrice);
+    }
+
+    private boolean isWhippedCreamSelected() {
+        return ((CheckBox) findViewById(R.id.cream_checkbox)).isChecked();
+    }
+
+    private boolean isChocolateSelected() {
+        return ((CheckBox) findViewById(R.id.chocolate_checkbox)).isChecked();
+    }
+
+    private void sendOrderByMail() {
+        String[] TO = {"your_mail@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("message/rfc822");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_order));
+        emailIntent.putExtra(Intent.EXTRA_TEXT, createOrderSummary());
+
+        if (emailIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(emailIntent);
+        }
     }
 }
